d264f1f03d2613d595c537539842d48b7c70fb0d
Matheus Teixeira <matheustexeirablu@hotmail.com>
2021-11-01 00:09:33 -0300

    up

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,8 +2,10 @@
 
 //console.log(utils(1,1))
 
+const validator = require('validator')
 const notes = require('./notes')
 
 var myNotes = 'Your Notes...'
-
 console.log(notes(myNotes))
+
+console.log(validator.isURL('https:/google.com'))
