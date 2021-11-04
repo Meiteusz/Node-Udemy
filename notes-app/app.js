ee7055eade34c96aa7c53a91eafe7debfc5fdfaf
Meiteusz <contato.matheuswt@gmail.com>
2021-11-03 22:57:44 -0300

    up

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
 const chalk = require('chalk')
-const notes = require('./notes')
+const getNotes = require('./notes')
+const addNotes = require('./notes')
+
+const command = process.argv[2];
 
 //Define my own themes
 const error = chalk.bold.inverse.red
@@ -9,11 +12,18 @@ const errorMessage = chalk.bold.red
 const warningMessage = chalk.keyword('orange')
 //--------------------
 
-var myNotes = 'Your Notes...'
-console.log(notes(myNotes))
-
 //Printing with color
-console.log(error('TEST ERROR'))
-console.log(warning('TEST WARNING'))
-console.log(errorMessage('TEST ERROR Message'))
-console.log(warningMessage('TEST WARNING Message'))
+//console.log(error('TEST ERROR'))
+//console.log(warning('TEST WARNING'))
+//console.log(errorMessage('TEST ERROR Message'))
+//console.log(warningMessage('TEST WARNING Message'))
+
+
+if(command === 'add'){
+    const response = addNotes(' test')
+    console.log(response)
+}else if(command === 'read'){
+    console.log(getNotes())
+}else if(command === 'remove'){
+    console.log('processing...remove notes')
+}
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,39 @@
-const getNotes = (notes)=>{
-    return notes
+const fs = require('fs')
+
+var response = {
+    Success: false,
+    Message: ''
+}
+
+const getNotes = ()=>{
+    return fs.readFileSync('notes-app/notes.txt', 'utf8')
+}
+
+const addNotes = (notes) =>{
+    try{
+
+        fs.appendFileSync('notes-app/notes.txt', notes)
+
+        return response = {
+            Success: true,
+            Message: 'Note added'
+        }
+
+    }catch{
+        return response = {
+            Success: false,
+            Message: 'error occurred'
+        }
+    }
+}
+
+const removeNotes = () =>{
+
+}
+
+const updateNotes = () =>{
+    
 }
 
 module.exports = getNotes
+module.exports = addNotes
